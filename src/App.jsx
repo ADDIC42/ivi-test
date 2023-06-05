82fad9b40fef758690957b5ff3ab6937a28db670
saraliev <adamfuturama@mail.ru>
2023-06-05 08:28:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Diamond from './components/Diamond/Diamond';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Diamond />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Diamond/Diamond.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import styles from "./Diamond.module.scss";
+
+const Diamond = () => {
+  const [count, setCount] = useState(1);
+  const [isCounting, setIsCounting] = useState(false);
+  const [showCount, setShowCount] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isCounting) {
+      interval = setInterval(() => {
+        if (count === 3) {
+          setIsCounting(false);
+          setShowCount(false);
+        } else {
+          setCount(count + 1);
+          setShowCount(true);
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count, isCounting]);
+
+  const handleIconClick = () => {
+    setIsCounting(true);
+    setShowCount(true);
+    setCount(1);
+  };
+
+  const handleMouseEnter = () => {
+    if (isCounting) {
+      document.body.style.backgroundColor = "#d5c40d";
+    }
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    document.body.style.backgroundColor = "white";
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    if (!isCounting && !isHovered) {
+      document.body.style.backgroundColor = "white";
+    }
+  }, [isCounting, isHovered]);
+
+  return (
+    <div
+      className={styles.Container_diamond}
+      onClick={handleIconClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className={styles.diamond}>
+        <div className={styles.cutDiamond}>
+          {showCount && <span className={styles.text}>{count}</span>}
+          {!showCount && <span className={styles.text}>Go!</span>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Diamond;
